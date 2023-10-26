d09fe84ccbfab29593fe925f5452da0cb1275113
mentalbergmite <124631600+mentalbergmite@users.noreply.github.com>
2023-10-25 22:25:53 -0400

    login
    begin laying out functions and main

--- client.py
+++ client.py
@@ -3,10 +3,10 @@ import time, json
 
 # items for database are notes
 # note includes: {id: unique number not null, noter_name: string not null, note: string not null, date_created: date not null}
-
+# Socket sends end in with the token unless for login
 ssock = None
 tokens = None
-
+dictionary = {"status":"", "token:":""}
 recvbufsize = 1024
 
 ACTIONS = ('LOGIN', 'LOGOUT', 'ADD', 'RETRIEVE', 'DELETE', 'CANCEL')
@@ -32,10 +32,12 @@ def closeConnection():
 def loginByID(id):
   status = None
   token = None 
-  # send a message to server with id
-  # make a json format from a dict
-  # message contains token and status
-  # extract status and token
+  ssock.send('LOGIN'+ id)  # send a message to server with id
+   #make sure the server sends the data in this order
+  data = json.loads(dictionary)# make a json format from a dict
+  data = ssock.recv(4096) # message contains token and status
+  status = data["status"]  # extract status and token
+  token = data["token"]
   # status, token = contact the server
   return status, token
 
@@ -43,12 +45,14 @@ def loginByID(id):
 def performAction(action, token):
   status = None
   message = None #response message
-  # send message with action
-  # return status, message
+  ssock.send(action+" "+token)# send message with action, space used as delimiter
+  return status,message # return status, message
+
 def logout(token):
   status = None
   message = None
-  # return status, message
+  ssock.send(ACTIONS[1]+" "+token)
+  return status,message# return status, message
   
 def retrieve(noteId, token):
   status = None
@@ -65,7 +69,7 @@ def add(noteName, noteMessage, token):
 def delete(noteId, token):
   status = None
   message = None # response message
-  # send a message to server to delete note
+  ssock.send(ACTIONS[4]+" "+noteId+" "+token)# send a message to server to delete note
   # return status, message for that status
   
 
@@ -73,4 +77,18 @@ def delete(noteId, token):
 
 # the rest is the driver code client
   
-  
+  if __name__ == '__main__':
+    try:
+      openConnection()
+    except socket.error:
+      print("Hostname not resolved")
+      exit()
+    keyword= ''
+    keyword = input("Enter ID") #PHASE 1
+    status,tokens =  loginByID(keyword)
+    if status != None:
+      print("Login successful")
+    while keyword.upper() != ACTIONS(1):#PHASE 2
+      pass
+    logout(tokens)  #PHASE 3
+    closeConnection()
